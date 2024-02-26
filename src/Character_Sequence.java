2a90de0976d9d25861f9b86756fa47644bef4298
VGoychev <144837107+VGoychev@users.noreply.github.com>
2024-02-26 23:30:04 +0200

    Initial commit

---
+++ src/Character_Sequence.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Character_Sequence {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        for (int index = 0; index < text.length(); index++) {
+           char symbol = text.charAt(index);
+            System.out.println(symbol);
+        }
+    }
+}
